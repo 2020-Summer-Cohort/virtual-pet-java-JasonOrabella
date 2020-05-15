39e4abbb527b5bf9199b9040b032f04a8d970cc7
Jason Orabella <jasonOrabella@gmail.com>
2020-05-15 15:43:59 -0400

    Test

---
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -0,0 +1,66 @@
+package virtual_pet;
+
+public class virtualPet {
+    void tick() {
+        int gameTick = 1;
+        return gameTick
+    }
+
+    public int getHunger() {
+
+    }
+
+    void getThirst() {
+    }
+
+    void getWaste() {
+
+    }
+
+    void getBoredom() {
+
+    }
+
+    void getPlay() {
+
+    }
+
+    public int getTiredness() {
+        int Tiredness = 0;
+        return Tiredness;
+
+    }
+
+    public int getEat() {
+    }
+}
+
+//public class VirtualPet {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//        VirtualPet petMane = new VirtualPet();
+//        /*
+//       hunger (feed it)
+//thirst (water it)
+//waste (let it out to the bathroom)
+//boredom (play with it)
+//sickness (take it to the doctor)
+//
+//
+//         */
+//
+//        public class Hunger {
+//            Hunger objName = new Hunger();
+//        }
+//
+//        public class Thirst {
+//            Thirst objName = new Thirst();
+//        }
+////
+//
+//    )
+//        }
+//    }
+//
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,8 +1,24 @@
 package virtual_pet;
 
+import java.util.Scanner;
+
 public class VirtualPetApplication {
 
     public static void main(String[] args) {
+        //will have a loop
+        Scanner input = new Scanner(system.in);
+        virtualPet virtualPet = new VirtualPet();
+        boolean gamerun = true;
+        while (gamerun) {
+            System.out.println("What would you like PetMane to do?");
+            System.out.println("1. Play");
+            System.out.println("2. Eat");
+
+            int getAction = input.nextInt();
+            if (getAction == 0) {
+                gamerun = false;
+            }
+        }
         //Interact with a VirtualPet object in this method
     }
 
